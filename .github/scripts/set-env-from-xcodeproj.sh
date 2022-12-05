d514d5068cc388a695d5c54d975a5a3669c068ae
Kara <kara.wilson.2005.08@gmail.com>
2022-12-04 22:51:52 -0500

    fix set-env-from-xcodeproj.sh

--- .github/scripts/set-env-from-xcodeproj.sh
+++ .github/scripts/set-env-from-xcodeproj.sh
@@ -3,5 +3,5 @@
 set -euo pipefail
 
 SCHEME="$(xcodebuild -list -json | jq -r '.project.schemes[0]')"
-PRODUCT_NAME="$(xcodebuild -scheme "$SCHEME" -showBuildSettings | grep " PRODUCT_NAME " | sed "s/[ ]*PRODUCT_NAME = //")"
+PRODUCT_NAME="$(xcodebuild -scheme "ios/$SCHEME" -showBuildSettings | grep " PRODUCT_NAME " | sed "s/[ ]*PRODUCT_NAME = //")"
 echo "::set-env name=PRODUCT_NAME::$PRODUCT_NAME"
