057c5c19ab02b0467e94af4e677012c08e9d6f1d
Kara <kara.wilson.2005.08@gmail.com>
2022-12-04 23:15:33 -0500

    fix set-env depreciation issue

--- .github/scripts/set-env-from-xcodeproj.sh
+++ .github/scripts/set-env-from-xcodeproj.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 SCHEME="$(xcodebuild -list -json | jq -r '.project.schemes[0]')"
 PRODUCT_NAME="$(xcodebuild -scheme "$SCHEME" -showBuildSettings | grep " PRODUCT_NAME " | sed "s/[ ]*PRODUCT_NAME = //")"
-echo "::set-env name=PRODUCT_NAME::$PRODUCT_NAME"
+echo "{name}=$PRODUCT_NAME" >> $GITHUB_ENV
