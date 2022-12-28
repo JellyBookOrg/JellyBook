5639038bb4fe8eeddbd23e2869a4fe5f2d247dfb
Kara <kara.wilson.2005.08@gmail.com>
2022-12-28 10:04:51 -0500

    Made improvements to for loops in `cbrCbzReader`

--- lib/screens/readingScreens/cbrCbzReader.dart
+++ lib/screens/readingScreens/cbrCbzReader.dart
@@ -42,15 +42,12 @@ class _CbrCbzReaderState extends State<CbrCbzReader> {
     List<String> pageFiles = [];
     for (var chapter in chapters) {
       List<FileSystemEntity> files = Directory(chapter).listSync();
-      for (var file in files) {
-        if (file.path.endsWith(formats[0]) ||
-            file.path.endsWith(formats[1]) ||
-            file.path.endsWith(formats[2]) ||
-            file.path.endsWith(formats[3]) ||
-            file.path.endsWith(formats[4]) ||
-            file.path.endsWith(formats[5]) ||
-            file.path.endsWith(formats[6])) {
-          pageFiles.add(file.path);
+      if (formats.any((element) => chapter.endsWith(element))) {
+        pageFiles.add(chapter);
+      } else {
+        List<FileSystemEntity> files = Directory(chapter).listSync();
+        for (var file in files) {
+          getChaptersFromDirectory(file);
         }
       }
     }
@@ -144,92 +141,37 @@ class _CbrCbzReaderState extends State<CbrCbzReader> {
     // what we want to do is recursively go through the folder and get all the last directories that dont contain any other directories
     // then we want to add them to the chapters list
     // max depth of 3
-    // the way we will do the checking is by checkign to see if the file is ends with a jpg, jpeg, png...
-    var formats = ['jpg', 'jpeg', 'png', 'gif', 'webp', 'bmp', 'tiff'];
-    if (file.path.endsWith(formats[0]) ||
-        file.path.endsWith(formats[1]) ||
-        file.path.endsWith(formats[2]) ||
-        file.path.endsWith(formats[3]) ||
-        file.path.endsWith(formats[4]) ||
-        file.path.endsWith(formats[5]) ||
-        file.path.endsWith(formats[6])) {
-      // debugPrint("file: $file");
-      if (!chapters.contains(file.parent.path)) {
-        chapters.add(file.parent.path);
-      }
-    } else {
-      // debugPrint("file: $file");
-      List<FileSystemEntity> files2 = Directory(file.path).listSync();
-      for (var file2 in files2) {
-        if (file2.path.endsWith(formats[0]) ||
-            file2.path.endsWith(formats[1]) ||
-            file2.path.endsWith(formats[2]) ||
-            file2.path.endsWith(formats[3]) ||
-            file2.path.endsWith(formats[4]) ||
-            file2.path.endsWith(formats[5]) ||
-            file2.path.endsWith(formats[6])) {
-          // debugPrint("file2: $file2");
-          if (!chapters.contains(file2.parent.path)) {
-            chapters.add(file2.parent.path);
-          }
-        } else {
-          // debugPrint("file2: $file2");
-          List<FileSystemEntity> files3 = Directory(file2.path).listSync();
-          for (var file3 in files3) {
-            if (file3.path.endsWith(formats[0]) ||
-                file3.path.endsWith(formats[1]) ||
-                file3.path.endsWith(formats[2]) ||
-                file3.path.endsWith(formats[3]) ||
-                file3.path.endsWith(formats[4]) ||
-                file3.path.endsWith(formats[5]) ||
-                file3.path.endsWith(formats[6])) {
-              // debugPrint("file3: $file3");
-              if (!chapters.contains(file3.parent.path)) {
-                chapters.add(file3.parent.path);
-              }
-            } else {
-              // debugPrint("file3: $file3");
-              List<FileSystemEntity> files4 = Directory(file3.path).listSync();
-              for (var file4 in files4) {
-                if (file4.path.endsWith(formats[0]) ||
-                    file4.path.endsWith(formats[1]) ||
-                    file4.path.endsWith(formats[2]) ||
-                    file4.path.endsWith(formats[3]) ||
-                    file4.path.endsWith(formats[4]) ||
-                    file4.path.endsWith(formats[5]) ||
-                    file4.path.endsWith(formats[6])) {
-                  // debugPrint("file4: $file4");
-                  if (!chapters.contains(file4.parent.path)) {
-                    chapters.add(file4.parent.path);
-                  }
-                } else {
-                  // debugPrint("file4: $file4");
-                  List<FileSystemEntity> files5 =
-                      Directory(file4.path).listSync();
-                  for (var file5 in files5) {
-                    if (file5.path.endsWith(formats[0]) ||
-                        file5.path.endsWith(formats[1]) ||
-                        file5.path.endsWith(formats[2]) ||
-                        file5.path.endsWith(formats[3]) ||
-                        file5.path.endsWith(formats[4]) ||
-                        file5.path.endsWith(formats[5]) ||
-                        file5.path.endsWith(formats[6])) {
-                      // debugPrint("file5: $file5");
-                      if (!chapters.contains(file5.parent.path)) {
-                        chapters.add(file5.parent.path + "/");
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
+    getChaptersFromDirectory(file);
 
     debugPrint("Chapters:");
     debugPrint(chapters.toString());
+  }
+
+  void getChaptersFromDirectory(FileSystemEntity directory) {
+    // Create a list of file types to check against
+    List<String> fileTypes = [
+      '.jpg',
+      '.jpeg',
+      '.png',
+      '.gif',
+      '.webp',
+      '.bmp',
+      '.tiff'
+    ];
+
+    // Check if the directory ends with any of the file types
+    if (fileTypes.any((fileType) => directory.path.endsWith(fileType))) {
+      // If it does, add the parent directory to the chapters list if it's not already there
+      if (!chapters.contains(directory.parent.path)) {
+        chapters.add(directory.parent.path);
+      }
+    } else {
+      // If it doesn't, recursively check the files in the directory
+      List<FileSystemEntity> files = Directory(directory.path).listSync();
+      for (var file in files) {
+        getChaptersFromDirectory(file);
+      }
+    }
   }
 
   @override
