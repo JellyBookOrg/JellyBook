5373cc772002b87857061c83c4b75a9cb13e2cf8
Kara <kara.wilson.2005.08@gmail.com>
2023-01-24 11:31:30 -0500

    remove unnecessary User-Agent

--- lib/screens/downloaderScreen.dart
+++ lib/screens/downloaderScreen.dart
@@ -102,8 +102,6 @@ class _DownloadScreenState extends State<DownloadScreen> {
         'Accept-Language': 'en-US,en;q=0.5',
         'Connection': 'keep-alive',
         'Upgrade-Insecure-Requests': '1',
-        'User-Agent':
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0',
       };
       url = url + '/Items/' + comicId + '/Download?api_key=' + token;
       var files = await Directory(dirLocation).list().toList();
