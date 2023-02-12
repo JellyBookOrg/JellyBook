ead39dea2568d6987e660ddbc7d66abc9b35d503
Kara <kara.wilson.2005.08@gmail.com>
2023-02-12 14:15:33 -0500

    group to ensure working properly

--- lib/screens/MainScreens/continueReadingScreen.dart
+++ lib/screens/MainScreens/continueReadingScreen.dart
@@ -28,14 +28,21 @@ class _ContinueReadingScreenState extends State<ContinueReadingScreen> {
     var entries = await isar!.entrys
         .where()
         .filter()
+        .group((q) => q.downloadedEqualTo(true).and().pageNumGreaterThan(0))
         .downloadedEqualTo(true)
         .and()
         .pageNumGreaterThan(0)
         .or()
-        .epubCfiIsNotEmpty()
+        .group((q) => q.epubCfiIsNotEmpty().and().downloadedEqualTo(true))
         .findAll();
     // convert the entries to a list
     return entries;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getEntries();
   }
 
   @override
