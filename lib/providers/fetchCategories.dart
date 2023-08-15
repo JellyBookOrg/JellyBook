16b3018aa67e3a7b70ae156817e4fccba1cb9118
Kara-Zor-El <kara.wilson.2005.08@gmail.com>
2023-08-15 19:20:40 -0400

    optimize offline book reader

--- lib/providers/fetchCategories.dart
+++ lib/providers/fetchCategories.dart
@@ -279,7 +279,7 @@ Future<List<String>> chooseCategories(List<String> categories, context) async {
   return wantedCategories;
 }
 
-Future<Pair> getServerCategoriesOffline(context) async {
+Future<(List<Entry>, List<Folder>)> getServerCategoriesOffline(context) async {
   logger.d("getting server categories");
   // final p_info.PackageInfo packageInfo =
   //     await p_info.PackageInfo.fromPlatform();
@@ -297,7 +297,7 @@ Future<Pair> getServerCategoriesOffline(context) async {
   if (hasComics) {
     // get the comics and folders from the database
     final isar = Isar.getInstance();
-    List<Entry> entries = await isar!.entrys
+    List<Entry> unlikedEntries = await isar!.entrys
         .where()
         .filter()
         .group((q) {
@@ -310,69 +310,36 @@ Future<Pair> getServerCategoriesOffline(context) async {
         })
         .and()
         .downloadedEqualTo(true)
+        .and()
+        .isFavoritedEqualTo(false)
         .findAll();
+
+    List<Entry> likedEntries = await isar!.entrys
+        .where()
+        .filter()
+        .group((q) {
+          return q
+              .typeEqualTo(EntryType.book)
+              .or()
+              .typeEqualTo(EntryType.comic)
+              .or()
+              .typeEqualTo(EntryType.audiobook);
+        })
+        .and()
+        .downloadedEqualTo(true)
+        .and()
+        .isFavoritedEqualTo(true)
+        .findAll();
+    // sort by title
+    likedEntries.sort((a, b) => a.title.compareTo(b.title));
+    unlikedEntries.sort((a, b) => a.title.compareTo(b.title));
+
+    List<Entry> entries = likedEntries + unlikedEntries;
+
     List<Folder> folders = await isar.folders.where().findAll();
     logger.d("got entries and folders");
     logger.d(folders.toString());
 
-    // turn into a map
-    List<Map<String, dynamic>> folderMap = [];
-    for (int i = 0; i < folders.length; i++) {
-      // go through each categories list of entries and see if it is downloaded, if it is, add it to the list, if not, don't
-      List<String> entriesList = [];
-      for (int j = 0; j < folders[i].bookIds.length; j++) {
-        if (entries
-            .map((entry) => entry.id)
-            .toList()
-            .contains(folders[i].bookIds[j])) {
-          entriesList.add(folders[i].bookIds[j]);
-        }
-      }
-      if (entriesList.isNotEmpty) {
-        folderMap.add({
-          "id": folders[i].id,
-          "name": folders[i].name,
-          "entries": entriesList,
-          "image": folders[i].image,
-        });
-      }
-    }
-
-    List<Map<String, dynamic>> comics = [];
-    entries.forEach((book) {
-      logger.d(book.title);
-      comics.add({
-        'id': book.id,
-        'name': book.title,
-        // 'imagePath':
-        //     '$url/Items/${book.id}/Images/Primary?&quality=90&Tag=${book.imageTags!['Primary']}',
-        // // if the imageTags!['Primary'] is null then we use 'Asset' instead
-        'imagePath': book.imagePath,
-        'releaseDate': book.releaseDate,
-        'path': book.path,
-        'description': book.description,
-        'url': book.url,
-        'communityRating': book.rating,
-        if (book.type == EntryType.comic || book.type == EntryType.book)
-          'type': book.path.toString().split('.').last.toLowerCase(),
-        'tags': book.tags,
-        'parent': book.parentId,
-        'isFavorite': book.isFavorited,
-        'isDownloaded': book.downloaded,
-      });
-    });
-
-    // organize the comics first by if liked and then alphabetically
-    comics.sort((a, b) {
-      if (a['isFavorite'] == b['isFavorite']) {
-        return a['name'].compareTo(b['name']);
-      } else if (a['isFavorite'] == true) {
-        return -1;
-      } else {
-        return 1;
-      }
-    });
-
-    return Pair(comics, folderMap);
+    return (entries, folders);
   }
 }
