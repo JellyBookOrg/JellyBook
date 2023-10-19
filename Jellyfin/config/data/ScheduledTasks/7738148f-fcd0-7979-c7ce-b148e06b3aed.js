09f01fac8df149d6821bd3c1ad0632ab8f46b0d6
Kara-Zor-El <kara.wilson.2005.08@gmail.com>
2023-10-19 01:47:35 -0400

    Update Jellyfin files

--- Jellyfin/config/data/ScheduledTasks/7738148f-fcd0-7979-c7ce-b148e06b3aed.js
+++ Jellyfin/config/data/ScheduledTasks/7738148f-fcd0-7979-c7ce-b148e06b3aed.js
@@ -1,1 +1,1 @@
-{"StartTimeUtc":"2023-10-17T19:12:34.5343296Z","EndTimeUtc":"2023-10-17T19:12:35.4785717Z","Status":"Completed","Name":"Scan Media Library","Key":"RefreshLibrary","Id":"7738148ffcd07979c7ceb148e06b3aed"}
+{"StartTimeUtc":"2023-10-18T21:49:22.7798449Z","EndTimeUtc":"2023-10-18T21:49:22.9845779Z","Status":"Completed","Name":"Scan Media Library","Key":"RefreshLibrary","Id":"7738148ffcd07979c7ceb148e06b3aed"}
